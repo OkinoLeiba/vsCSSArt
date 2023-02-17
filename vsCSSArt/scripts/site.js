fff45d8a84ae22839fa0ea8dad7f3468f288ccf9
Okino Kamali Leiba <17099286+OkinoLeiba@users.noreply.github.com>
2023-02-17 01:32:11 -0500

    Add project files.

---
+++ vsCSSArt/scripts/site.js
@@ -0,0 +1,18 @@
+﻿function toggleCSS() {
+
+    var toggleElementBackground = document.getElementById("phone-screen-background");
+    var toggleElementForeground = document.getElementById("front-camera-circle");
+    var toggeleElementFingerPrint = document.getElementById("bt-inv-center-toggle");
+    //toggleElement.classList.toggle("phone-screen-background");
+
+    if (toggleElementBackground.style.display === "none" && toggleElementForeground.style.display === "block") {
+        toggleElementBackground.style.display = "block";
+        toggleElementForeground.style.display = "none";
+        toggeleElementFingerPrint.style.display = "none";
+    } else {
+        toggleElementBackground.style.display = "none";
+        toggleElementForeground.style.display = "block";
+        toggeleElementFingerPrint.style.display = "block";
+    }
+   
+}
