a8b1e974df65ed2aaa6020aa57faf395a8c01ff6
Okino Kamali Leiba <17099286+OkinoLeiba@users.noreply.github.com>
2023-02-18 20:53:09 -0500

    commit updates

--- vsCSSArt/scripts/site.js
+++ vsCSSArt/scripts/site.js
@@ -1,18 +1,21 @@
 ﻿function toggleCSS() {
 
-    var toggleElementBackground = document.getElementById("phone-screen-background");
+    //var toggleElementBackground = document.getElementById("phone-screen-background");
     var toggleElementForeground = document.getElementById("front-camera-circle");
     var toggeleElementFingerPrint = document.getElementById("bt-inv-center-toggle");
+    var toggeleElementLandingScreen = document.getElementById("bg-group-landing_screen");
     //toggleElement.classList.toggle("phone-screen-background");
 
-    if (toggleElementBackground.style.display === "none" && toggleElementForeground.style.display === "block") {
-        toggleElementBackground.style.display = "block";
+    if (toggeleElementLandingScreen.style.display === "none" && toggleElementForeground.style.display === "block") {
+        //toggleElementBackground.style.display = "block";
         toggleElementForeground.style.display = "none";
         toggeleElementFingerPrint.style.display = "none";
+        toggeleElementLandingScreen.style.display = "block";
     } else {
-        toggleElementBackground.style.display = "none";
+        //toggleElementBackground.style.display = "none";
         toggleElementForeground.style.display = "block";
         toggeleElementFingerPrint.style.display = "block";
+        toggeleElementLandingScreen.style.display = "none";
     }
    
 }
